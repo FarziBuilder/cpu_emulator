5ad3ddead8e0e3ef325f519f5f1b3ed5a6ed7671
FarziBuilder <faraaz@elixarsystems.com>
2024-04-27 15:48:35 +0530

    final

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # cpu_emulator
-I can build my own CPUs
+27th April:- I can build my own CPUs
+
+# Specs
+This is a 8-bit CPU with 64 Kb of addressable memory, emulating 6502 microprocessor (OG used by Woz to make Apple 1).  
+This just does basic LDA and JSR instructions of the CPU i.e. it can go to a register value and execute the instruction and store in accumulator  
+It can also create a subroutine, and branch out to execute other instructions.  
+
+Gawd I am a genius (context-i had just solved a simple bug, lol!)
+
+# Some Notes  
+Emulating 6502 microprocessor
+- zero page :- First 256 bytes of the CPU
+- Program Counter;- Holds the memory address to the next line of instructions to be executed
+- Stack:- Reusable memory (256 bytes)
+- Stack Pointer:- 8 bit register holding the last 8 bits of the next free location of the stack
+- Processor Regsiter:- Quick accessible registers for storing imp data
+    - Input Register X,Y:- Stores the counter values
+    - Accumulator:- Stores the counter values (stores all logical operations , i guess)
+- Flags:- 1-bit register values to store the results of execution of an instruction (Carry, etc)
+
+Fns defined
+- Reset():- sets the RF, SP values + Sets the vakue of the Registers
+- Mem:- Stores
+- ReadWord for reading the 2 bytes after address of the opcode
+- WriteWord for storing requisite values to the stack pointer
+- FetchByte:- For reading a register value and incrementing PC. ReadByte:- For just reading and not incrementing
+
+Operators defined:-
+- [], used for accessing the memory class and changing the Data var inside it.
+
+In JSR, after address at which opcode is stored, the next 2 bytes are the addresses of the instructions of subroutine.
+
+---
+64 Kb addressable memory, 8-bit registers microprocessor
+Can't really do much except find specific memory locations, understand what to do with the upcoming registers, do those basic stuff.
+For making it do useful stuff, you need other I/O devices like screens, keyboards whose register values it will change to make them do useful stuff (like print hello)
+For making prog languages run on it. Make a compiler which can convert high-level prog to low-level instructions which can run on this CPU emulator (need to learn more here!)
--- try.cpp
+++ try.cpp
@@ -1,31 +1,10 @@
-/*
-- working on 6552 microprocessor
-- zero page :- First 256 bytes of the CPU
-- Program Counter;- Holds the memory address to the next line of instructions to be executed
-- Stack:- Reusable memory (256 bytes)
-- Stack Pointer:- 8 bit register holding the last 8 bits of the next free location of the stack
-- Processor Regsiter:- Quick accessible registers for storing imp data
-    - Input Register X,Y:- Stores the counter values
-    - Accumulator:- Stores the counter values (stores all logical operations , i guess)
-- Flags:- 1-bit register values to store the results of execution of an instruction (Carry, etc)
-
-Fns defined
-- Reset():- sets the RF, SP values + Sets the vakue of the Registers
-- Mem:- Stores
-
-In JSR, after address at which opcode is stored, the next 2 bytes are the addresses of the instructions of subroutine.
-
-*/
-
-
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
 
 using namespace std;
 
-const unsigned int MAX_MEM = 1024 * 64;
+const unsigned int MAX_MEM = 1024 * 64; //2^16
 using Byte = unsigned char;
 using Word = unsigned short;
 
@@ -42,6 +21,18 @@ struct Mem {
 
     Byte& operator[](u32 address) {
         return Data[address];
+    }
+
+    void WriteWord(
+        Word value, 
+        Word& Address,
+        u32& Cycles
+        )
+    {
+        Data[Address] = value & 0xFF; //PC is a word, so you need to store it this way
+        Data[Address + 1] = (value >> 8);
+        Address -= 1; //for moving the stack pointer after storing the location. Most prob it needs to be -1 rather than +1.
+        Cycles -= 2;
     }
 };
 
@@ -61,7 +52,8 @@ struct CPU {
 
     static constexpr Byte INS_LDA_IM = 0xA9, 
     INS_LDA_ZP = 0xA5,
-    INS_LDA_ZPX = 0xB5;
+    INS_LDA_ZPX = 0xB5,
+    INS_JSR = 0xB7; //static constexpr belong to class not the objects, hence can be referenced via CPU::INS_JSR!
 
     void Reset(Mem& mem) {
         PC = 0xFFFC;
@@ -90,14 +82,17 @@ struct CPU {
         N = ((A & 0b10000000) > 0);
         Z = (A == 0);
     }
-
+ 
     Word ReadWord(u32& cycle, Mem& memory)
     {
         Word Data = memory[PC];
         PC++;
         Data |= (memory[PC] << 8);
+        cycle -= 2;
         return Data;
     }
+
+    
 
     void Execute(u32 Cycles, Mem& memory) {
         while (Cycles > 0) {
@@ -110,16 +105,16 @@ struct CPU {
                     Byte Value = FetchByte(Cycles, memory);
                     A = Value;
                     LDASetStatus();
-                    cout << "check" <<endl;
-                    break;
-                }
+                    //cout << "check" <<endl;
+                    
+                } break;
                 case INS_LDA_ZP:
                 {
                     Byte Value = FetchByte(Cycles, memory); //so 3 cycles needed to run this instruction. First one has the address which has the opcode stored, 2nd one gives the address where the instruction is stored. 3rd one goes into the address where the final instruction is there, which is stored in accumulator
                     A = ReadByte(Cycles,Value, memory);
                     LDASetStatus();
-                    break;
-                }
+                    
+                } break;
                 case INS_LDA_ZPX:
                 {
                     Byte Value = FetchByte(Cycles, memory);
@@ -127,10 +122,19 @@ struct CPU {
                     Cycles--;
                     A = ReadByte(Cycles,Value, memory);
                     LDASetStatus();
-                    break;
-                }
+                    
+                } break;
+                case INS_JSR:
+                {
+                    Word instruc = ReadWord(Cycles, memory);
+                    memory.WriteWord(
+                        PC - 1, SP, Cycles);
+                    PC =  instruc;
+                    Cycles--;
+                    
+                } break;
                 default:
-                    cout << "hi baby" << endl;
+                    cout << "You are in default " << endl;
                     return;
             }
         }
@@ -142,20 +146,26 @@ int main() {
     CPU cpu;
     cpu.Reset(mem);
     //cpu.Execute(2, mem);
-    mem[0xFFFC] = CPU::INS_LDA_IM;
-    mem[0xFFFD] = 0x42;
-    cpu.Execute(2, mem);
-    cpu.Reset(mem);
-    mem[0xFFFC] = CPU::INS_LDA_ZP;
-    mem[0xFFFD] = 0x42;
-    mem[0x0042] = 0x84;
-    // cout << static_cast<unsigned int>(mem[0xFFFC]);
-    // if(mem[0xFFFC] == CPU::INS_LDA_IM){
-    //     cout << "Hey babe"<<endl;
-    // }else{
-    //     cout << "No";
-    // }
+
+
+    //Testing LDA_ZP
+    // mem[0xFFFC] = CPU::INS_LDA_IM;
     // mem[0xFFFD] = 0x42;
     // cpu.Execute(2, mem);
+    // cpu.Reset(mem);
+    // mem[0xFFFC] = CPU::INS_LDA_ZP;
+    // mem[0xFFFD] = 0x42;
+    // mem[0x0042] = 0x84;
+    // cpu.Reset(mem);
+    
+    //Testing INS_JSR
+    mem[0xFFFC] = CPU::INS_JSR;
+    mem[0xFFFD] = 0x42;
+    mem[0xFFFE] = 0x42;
+    mem[0x4242] = CPU::INS_LDA_IM;
+    mem[0x4243] = 0x84;
+    cpu.Execute(8, mem);
+    cout << static_cast<unsigned int>(cpu.A);
+    // cout << static_cast<unsigned int>(mem[0xFFFC]);
     return 0;
 }
